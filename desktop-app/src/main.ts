4619693ed2e082c592a12d412036c57596f5a8a5
lihuithe <lihuithe@users.noreply.github.com>
2026-05-10 03:51:02 +0000

    修复国际化问题并更新截图

--- desktop-app/src/main.ts
+++ desktop-app/src/main.ts
@@ -3,6 +3,7 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import pinia from './stores'
+import { i18n } from './locales'
 import { performanceMonitor } from './performance'
 
 const app = createApp(App)
@@ -11,5 +12,6 @@ performanceMonitor.start()
 
 app.use(pinia)
 app.use(router)
+app.use(i18n)
 
 app.mount('#app')
